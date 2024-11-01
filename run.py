44f1efc6229f258a01ef196a1f14eb89001aaedf
Kanika Poonia <64961276+Kanik-a@users.noreply.github.com>
2024-10-31 21:26:19 -0400

    Run.py

--- run.py
+++ run.py
@@ -1,74 +1,64 @@
-
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
 
 # These two lines make sure a faster SAT solver is used.
 from nnf import config
 config.sat_backend = "kissat"
-
-# Encoding that will store all of your constraints
+# Create an encoding to store constraints
 E = Encoding()
-
-# To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
 @proposition(E)
 class BasicPropositions:
 
     def __init__(self, data):
         self.data = data
 
-    def _prop_name(self):
+    def prop_name(self):
         return f"A.{self.data}"
+# Define basic propositions for the Uno game
+C = BasicPropositions("C")  # Previous card and the card in players hand have the same colour
+T = BasicPropositions("T")  # Previous card and the card in players hand have the same number
+B = BasicPropositions("B")  # Player has a card that can be played
+E2 = BasicPropositions("E2") # Previous card is a draw 2 card
+F = BasicPropositions("F")  # Previous card is a draw 4 card
+R = BasicPropositions("R")  # Previous card is a reverse card
+S = BasicPropositions("S")  # Previous card is a skip card
+U = BasicPropositions("U")  # Player has met the winning condition
+W = BasicPropositions("W")  # Wild card can be played
+A = BasicPropositions("A")  # Player has cards in hand
+D = BasicPropositions("D")  # Player has to draw a card
 
-
-# Different classes for propositions are useful because this allows for more dynamic constraint creation
-# for propositions within that class. For example, you can enforce that "at least one" of the propositions
-# that are instances of this class must be true by using a @constraint decorator.
-# other options include: at most one, exactly one, at most k, and implies all.
-# For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
+# Define a fancy proposition for checking if a card can be played
+prop = [C,T,B,E2,F,R,S,U,W,A,D]
 @constraint.at_least_one(E)
 @proposition(E)
-class FancyPropositions:
+class FancyProposition:
 
     def __init__(self, data):
         self.data = data
 
     def _prop_name(self):
-        return f"A.{self.data}"
+        return f"Playable.{self.data}"
 
-# Call your variables whatever you want
-a = BasicPropositions("a")
-b = BasicPropositions("b")   
-c = BasicPropositions("c")
-d = BasicPropositions("d")
-e = BasicPropositions("e")
-# At least one of these will be true
-x = FancyPropositions("x")
-y = FancyPropositions("y")
-z = FancyPropositions("z")
+x = FancyProposition("x")
+def uno_constraints():
+    
+    # Add constraints for the playable condition
+    E.add_constraint(B >> (C | T))                # B implies C or T
+    E.add_constraint(A & (C | T | W) >> B)        # A and (C or T or W) implies B
+    E.add_constraint((R | S) >> ~B)                # (R or S) implies not B
+    E.add_constraint((F | E2) >> (D | B))           # (F or E) implies (D or B)
+    E.add_constraint(~A >> U)                      # not A implies U
+    E.add_constraint(~B >> D)                      # not B implies D
+    E.add_constraint(W >> (C | T) & ~(C | T)) 
 
-
-# Build an example full theory for your setting and return it.
-#
-#  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
-#  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
-#  what the expectations are.
-def example_theory():
-    # Add custom constraints by creating formulas with the variables you created. 
-    E.add_constraint((a | b) & ~x)
-    # Implication
-    E.add_constraint(y >> z)
-    # Negate a formula
-    E.add_constraint(~(x & y))
-    # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
-    # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    constraint.add_exactly_one(E, a, b, c)
+    # At least one of the previous card conditions must be true
+    #E.add_constraint(constraint.at_least_one(E2))
 
     return E
 
-
 if __name__ == "__main__":
 
-    T = example_theory()
+    T = uno_constraints()
     # Don't compile until you're finished adding all your constraints!
     T = T.compile()
     # After compilation (and only after), you can check some of the properties
@@ -78,7 +68,7 @@ if __name__ == "__main__":
     print("   Solution: %s" % T.solve())
 
     print("\nVariable likelihoods:")
-    for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
+    for v,vn in zip([C,T,B,E2,F,R,S,U,W,A,D], 'abcxyz'):
         # Ensure that you only send these functions NNF formulas
         # Literals are compiled to NNF here
         print(" %s: %.2f" % (vn, likelihood(T, v)))
